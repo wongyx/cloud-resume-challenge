9e3ea5a2564fa737009ba6a2dea4c28811b839c4
wong_yx <wongyongxiang00@gmail.com>
2026-01-31 06:14:12 -0500

    Add GetObjectVersion perms

--- infrastructure/modules/backend/iam.tf
+++ infrastructure/modules/backend/iam.tf
@@ -107,6 +107,7 @@ resource "aws_iam_policy" "github_actions" {
         Action = [
           "s3:PutObject",
           "s3:GetObject",
+          "s3:GetObjectVersion",
           "s3:DeleteObject",
           "s3:ListBucket",
           "s3:ListBucketVersions"
